a3ceda0f3d60b3b97a6bf4e484c9249d1402e144
Ralf <ralflopez3@gmail.com>
2022-05-07 09:09:24 +0800

    :card_file_box: set email property to unqiue

--- schema.sql
+++ schema.sql
@@ -6,9 +6,9 @@ CREATE TYPE Role AS ENUM (
 
 DROP TABLE IF EXISTS "users";
 CREATE TABLE "public"."users" (
-    "id" uuid NOT NULL,
+    "id" uuid NOT NULL PRIMARY KEY,
     "name" character varying(255) NOT NULL,
-    "email" character varying(255) NOT NULL,
+    "email" character varying(255) NOT NULL UNIQUE,
     "password" text NOT NULL,
     "role" role DEFAULT Member NOT NULL,
     "created_at" timestamptz DEFAULT now(),
